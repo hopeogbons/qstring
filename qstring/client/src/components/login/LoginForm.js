602746611048e48a9ef60395e0f0ccd071411307
hope-ogbons <hopeogbons@gmail.com>
2018-09-04 17:36:52 +0100

    updated css

--- qstring/client/src/components/login/LoginForm.js
+++ qstring/client/src/components/login/LoginForm.js
@@ -7,13 +7,13 @@ class LoginForm extends Component {
       <div className={"text-center"}>
         <div className={"mx-auto"} style={{marginTop:"150px",width:"300px"}}>
           <form className="form-signin">
-            <img className="mb-4" src={logo} alt="" height="50px"/>
+            <img className="mb-4" src={logo} alt="Logo" height="50px"/>
             <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
             <label htmlFor="inputEmail" className="sr-only">Email address</label>
             <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
             <label htmlFor="inputPassword" className="sr-only">Password</label>
             <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-            <div className="checkbox mb-3">
+            <div className="checkbox my-3">
               <label>
                 <input type="checkbox" value="remember-me" /> Remember me
               </label>
