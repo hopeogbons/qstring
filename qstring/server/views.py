9f5afb4b6bac00a7d4f8245826be9afda870ee80
hope-ogbons <hopeogbons@gmail.com>
2018-10-02 01:28:42 +0100

    added login and logout views

--- qstring/server/views.py
+++ qstring/server/views.py
@@ -1,6 +1,39 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from qstring.server.serializers import UserSerializer, GroupSerializer
+from rest_framework.views import APIView
+from qstring.server.serializers import UserSerializer, GroupSerializer, LoginSerializer
+from django.contrib.auth import login as django_login, logout as django_logout
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from django.views.decorators.csrf import csrf_exempt
+
+
+class LoginView(APIView):
+    @staticmethod
+    @csrf_exempt
+    def post(request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        django_login(request, user)
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            "username": user.username,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "token": token.key
+        }, status=200)
+
+
+class LogoutView(APIView):
+    authentication_classes = (TokenAuthentication, )
+
+    @staticmethod
+    def post(request):
+        django_logout(request)
+        return Response(status=204)
 
 
 class UserViewSet(viewsets.ModelViewSet):
