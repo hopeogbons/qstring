1fb874221d3a9b55f7e6b44e80cff75c663034e3
hope-ogbons <hopeogbons@gmail.com>
2018-10-02 01:28:16 +0100

    added login and logout serializers

--- qstring/server/serializers.py
+++ qstring/server/serializers.py
@@ -1,5 +1,37 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from django.contrib.auth import authenticate
+from rest_framework import serializers, exceptions
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        username = data.get("username", "")
+        password = data.get("password", "")
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    data["user"] = user
+                else:
+                    msg = "User is deactivated"
+                    raise exceptions.ValidationError(msg)
+            else:
+                msg = "Unable to login with given credentials"
+                raise exceptions.ValidationError(msg)
+        else:
+            msg = "Must provide username and password"
+            raise exceptions.ValidationError(msg)
+        return data
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
