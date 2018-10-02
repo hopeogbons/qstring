5b0ecd3d89dccba1905c858ffe75f64e4125ff34
hope-ogbons <hopeogbons@gmail.com>
2018-10-02 01:26:33 +0100

    refactored reducer logic

--- qstring/client/src/reducers/index.js
+++ qstring/client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
-import counter from './counter'
+import { reducer as form } from 'redux-form';
+import user from './user'
 
 export default combineReducers({
-  counter
+  form: form,
+  user
 })
