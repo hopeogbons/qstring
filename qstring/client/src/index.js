d2165182e689a1bd227782d720dcd7c747b763e2
hope-ogbons <hopeogbons@gmail.com>
2018-10-08 00:39:51 +0100

    refactored the application index file

--- qstring/client/src/index.js
+++ qstring/client/src/index.js
@@ -1,30 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import $ from 'jquery';
-// import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
-import { Route, Switch } from 'react-router-dom';
 import store, { history } from './store/configureStore';
-import setAuthorizationToken from './utils/setAuthorizationToken';
-
 import 'sanitize.css/sanitize.css';
 import './index.css';
-
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-
-setAuthorizationToken(localStorage.getItem('qstring'));
+import App from './pages/App';
 
 render(
   <Provider store={ store }>
-    <ConnectedRouter history={ history }>
-      <Switch>
-        <Route exact path="/login" component={ Login } />
-        <Route exact path="/" component={ Dashboard } />
-      </Switch>
+    <ConnectedRouter history={history}>
+      <App history={history}/>
     </ConnectedRouter>
   </Provider>
   , document.querySelector('#root')
