b1af4027a9dc17cd13fc23c0de79aef902c1649f
hope-ogbons <hopeogbons@gmail.com>
2018-10-08 00:38:46 +0100

    refactored AppContainer logic

--- qstring/client/src/containers/AppContainer.js
+++ qstring/client/src/containers/AppContainer.js
@@ -1,4 +1,41 @@
 import { connect } from 'react-redux';
+import { push } from 'connected-react-router';
+import { withRouter } from 'react-router-dom';
+import {
+  userTokenAuthRequest,
+  userTokenAuthFailure,
+  userTokenAuthSuccess,
+  userTokenResetRequest
+} from '../actions/users';
 import App from '../components/App.js';
 
-export default connect(null, null)(App);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadUserFromToken: () => {
+      const token = localStorage.getItem('qstring');
+      dispatch(userTokenAuthRequest(token))
+        .then(res => {
+          const { token, error } = res.payload;
+          if (token) {
+            dispatch(userTokenAuthSuccess(token));
+            dispatch(push('/'));
+          } else {
+            dispatch(userTokenAuthFailure(error));
+            dispatch(userTokenResetRequest());
+            dispatch(push('/login'));
+          }
+        })
+        .catch(error => {
+          dispatch(userTokenAuthFailure(error));
+          dispatch(userTokenResetRequest());
+          dispatch(push('/login'));
+        })
+  	 },
+     resetUser: () =>{
+     	dispatch(userTokenResetRequest());
+     }
+  }
+}
+
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
