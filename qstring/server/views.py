f7e0665d371c851e3240cc4f939accaeba296936
hope-ogbons <hopeogbons@gmail.com>
2018-10-08 00:44:01 +0100

    trimmed the return data from the login view

--- qstring/server/views.py
+++ qstring/server/views.py
@@ -6,25 +6,19 @@ from django.contrib.auth import login as django_login, logout as django_logout
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.permissions import IsAuthenticated
+# from django.views.decorators.csrf import csrf_exempt
 
 
 class LoginView(APIView):
     @staticmethod
-    @csrf_exempt
     def post(request):
         serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data["user"]
         django_login(request, user)
         token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            "username": user.username,
-            "email": user.email,
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-            "token": token.key
-        }, status=200)
+        return Response({"token": token.key}, status=200)
 
 
 class LogoutView(APIView):
@@ -40,6 +34,8 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
