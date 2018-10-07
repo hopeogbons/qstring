88eb139bdc9736f46291a04b4ffcdee7a7bbf4e3
hope-ogbons <hopeogbons@gmail.com>
2018-10-08 00:40:18 +0100

    refactored App page logic

--- qstring/client/src/pages/App.js
+++ qstring/client/src/pages/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { Component } from 'react';
+import React, {Component} from 'react';
 import AppContainer from '../containers/AppContainer';
 
 export default class App extends Component {
   render() {
     return (
-    	<AppContainer>
-    	 {this.props.children}
-    	</AppContainer>
+      <div>
+        <AppContainer>
+          {this.props.children}
+        </AppContainer>
+      </div>
     );
   }
 }
