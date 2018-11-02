dfe58c0a45b400815c3af343bcf671701c356e84
hope-ogbons <hopeogbons@gmail.com>
2018-11-02 10:53:48 +0100

    updated header

--- qstring/client/src/components/Header.js
+++ qstring/client/src/components/Header.js
@@ -3,12 +3,32 @@ import React from "react";
 
 export default class Header extends Component {
 	render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
-        <div className="container">
-          <a className="navbar-brand" href="/">Qstring</a>
+    const status = this.props.status;
+    if (status === 'is_logged_in') {
+      return (
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+              <a className="navbar-brand" href="/">Qstring</a>
+              <ul className="nav justify-content-end">
+                <li className="nav-item">
+                  <a className="nav-link active text-white" href="javascript:void(0);" onClick={this.props.logout}>Logout</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </div>
-      </nav>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+              <a className="navbar-brand" href="/">Qstring</a>
+            </div>
+          </nav>
+        </div>
+      );
+    }
 	}
 }
