86efa8cd8f258189ba790856cdf558867d4c4335
hope-ogbons <hopeogbons@gmail.com>
2018-11-02 11:08:05 +0100

    removed the importation of csrf_exempt

--- qstring/server/views.py
+++ qstring/server/views.py
@@ -7,7 +7,6 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-# from django.views.decorators.csrf import csrf_exempt
 
 
 class LoginView(APIView):
