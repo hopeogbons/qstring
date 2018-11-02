79bc5775c13fdd0bf9ce8e6de6a1661f2ec899e3
hope-ogbons <hopeogbons@gmail.com>
2018-11-02 11:06:23 +0100

    renamed SurveyContainer to StringListContainer

--- qstring/client/src/pages/StringList.js
+++ qstring/client/src/pages/StringList.js
@@ -8,7 +8,7 @@ export default class Login extends Component {
     return (
       <div>
         <HeaderContainer status="is_logged_in"/>
-        <SurveyContainer/>
+        <StringListContainer/>
         <FooterContainer/>
       </div>
     );
