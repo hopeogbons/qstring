51b785ce5381da554d06b839091e46cf8200b276
hope-ogbons <hopeogbons@gmail.com>
2018-11-02 11:01:59 +0100

    updated header component

--- qstring/client/src/containers/HeaderContainer.js
+++ qstring/client/src/containers/HeaderContainer.js
@@ -1,4 +1,20 @@
 import { connect } from 'react-redux';
 import Header from '../components/Header';
+import {userLogoutReqest} from "../actions/users";
+import {push} from "connected-react-router";
 
-export default connect(null, null)(Header);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => {
+      dispatch(userLogoutReqest())
+        .then(() => {
+          dispatch(push('/login'));
+        })
+        .catch(() => {
+          dispatch(push('/'));
+        })
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Header);
