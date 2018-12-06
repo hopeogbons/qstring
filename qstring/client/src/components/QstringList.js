2ab92e82b5cfb01bc2915e2e72927c61d0b2d3d0
Hope Ogbons <hopeogbons@eHA-Hope-MacBook.local>
2018-12-06 16:23:30 +0100

    refactored entire logic

--- qstring/client/src/components/QstringList.js
+++ qstring/client/src/components/QstringList.js
@@ -1,114 +1,70 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class QstringList extends Component {
-  render() {
-    return (
-      <div>
-        <div className="nav-scroller bg-white shadow-sm">
-          <div className="container">
-            <div className="row py-2 px-3">
-              <button className="btn btn-info btn-sm ml-auto" onClick={this.props.gotoQstringForm}>+ New Qstring</button>
+  componentDidMount() {
+    this.props.fetchQstrings();
+  }
+
+  renderQstrings(qstrings) {
+    return qstrings.map(qs => {
+      return (
+        <div className="col-md-4 bg-white" key={qs.id}>
+          <div className="card mb-4 shadow-sm">
+            <div className="card-body">
+              <h4>{qs.title}</h4>
+              <h6>{qs.desc}</h6>
+              <p className="card-text"><span className="badge badge-pill badge-dark">{qs.count_submissions}</span> Submissions</p>
+              <div className="d-flex justify-content-between align-items-center row">
+                <div className="btn-group col">
+                  <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                  <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
+                </div>
+                <span className="text-muted col">Channel <span className="badge badge-pill badge-warning">{qs.id}</span></span>
+              </div>
             </div>
           </div>
         </div>
-        <main role="main">
-          <div className="container">
-            <div className="row" style={{marginTop: '50px'}}>
+      )
+    });
+  }
 
-              <div className="col-md-4 bg-white">
-                <div className="card mb-4 shadow-sm">
-                    <div className="card-body">
-                      <h4>SEPTEMBER 2018 IPD</h4>
-                      <h6>(20/09/2018 - 23/09/2018)</h6>
-                      <p className="card-text"><span className="badge badge-pill badge-dark">215</span> Reports</p>
-                      <div className="d-flex justify-content-between align-items-center row">
-                        <div className="btn-group col">
-                          <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
-                        </div>
-                        <span className="text-muted col">Channel <span className="badge badge-pill badge-warning">30</span></span>
-                      </div>
-                    </div>
-                </div>
+  render() {
+    const { data, error, loading } = this.props.qstrings;
+
+    if (loading) {
+      return (
+        <div className="container">
+          <h1>Qstrings</h1><h3>Loading...</h3>
+        </div>
+      )
+    } else if (error) {
+      return (
+        <div className="alert alert-danger">
+          Error: {error}
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <div className="nav-scroller bg-white shadow-sm">
+            <div className="container">
+              <div className="row py-2 px-3">
+                <button className="btn btn-info btn-sm ml-auto" onClick={this.props.loadQstringForm}>+ New Qstring</button>
               </div>
-
-              <div className="col-md-4 bg-white">
-                <div className="card mb-4 shadow-sm">
-                    <div className="card-body">
-                      <h4>SEPTEMBER 2018 IPD</h4>
-                      <h6>(20/09/2018 - 23/09/2018)</h6>
-                      <p className="card-text"><span className="badge badge-pill badge-dark">215</span> Reports</p>
-                      <div className="d-flex justify-content-between align-items-center row">
-                        <div className="btn-group col">
-                          <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
-                        </div>
-                        <span className="text-muted col">Channel <span className="badge badge-pill badge-warning">30</span></span>
-                      </div>
-                    </div>
-                </div>
-              </div>
-
-              <div className="col-md-4 bg-white">
-                <div className="card mb-4 shadow-sm">
-                    <div className="card-body">
-                      <h4>SEPTEMBER 2018 IPD</h4>
-                      <h6>(20/09/2018 - 23/09/2018)</h6>
-                      <p className="card-text"><span className="badge badge-pill badge-dark">215</span> Reports</p>
-                      <div className="d-flex justify-content-between align-items-center row">
-                        <div className="btn-group col">
-                          <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
-                        </div>
-                        <span className="text-muted col">Channel <span className="badge badge-pill badge-warning">30</span></span>
-                      </div>
-                    </div>
-                </div>
-              </div>
-
-              <div className="col-md-4 bg-white">
-                <div className="card mb-4 shadow-sm">
-                    <div className="card-body">
-                      <h4>SEPTEMBER 2018 IPD</h4>
-                      <h6>(20/09/2018 - 23/09/2018)</h6>
-                      <p className="card-text"><span className="badge badge-pill badge-dark">215</span> Reports</p>
-                      <div className="d-flex justify-content-between align-items-center row">
-                        <div className="btn-group col">
-                          <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
-                        </div>
-                        <span className="text-muted col">Channel <span className="badge badge-pill badge-warning">30</span></span>
-                      </div>
-                    </div>
-                </div>
-              </div>
-
-              <div className="col-md-4 bg-white">
-                <div className="card mb-4 shadow-sm">
-                    <div className="card-body">
-                      <h4>SEPTEMBER 2018 IPD</h4>
-                      <h6>(20/09/2018 - 23/09/2018)</h6>
-                      <p className="card-text"><span className="badge badge-pill badge-dark">215</span> Reports</p>
-                      <div className="d-flex justify-content-between align-items-center row">
-                        <div className="btn-group col">
-                          <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                          <button type="button" className="btn btn-sm btn-outline-secondary">Delete</button>
-                        </div>
-                        <span className="text-muted col">Channel <span className="badge badge-pill badge-warning">30</span></span>
-                      </div>
-                    </div>
-                </div>
-              </div>
-
             </div>
           </div>
-        </main>
-      </div>
-    )
+          <main role="main">
+            <div className="container">
+              <div className="row" style={{ marginTop: '50px' }}>
+
+                {this.renderQstrings(data)}
+
+              </div>
+            </div>
+          </main>
+        </div>
+      )
+    }
   }
 }
