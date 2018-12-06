98b56dc9023bcc655528c1d7a964fd895571c7f9
Hope Ogbons <hopeogbons@eHA-Hope-MacBook.local>
2018-12-06 16:31:11 +0100

    switched serializers from HyperlinkModelSerializer to ModelSerializer

--- qstring/server/serializers.py
+++ qstring/server/serializers.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User, Group
+from qstring.server.models import Qstring, Submission
 from django.contrib.auth import authenticate
 from rest_framework import serializers, exceptions
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class LoginSerializer(serializers.Serializer):
@@ -34,13 +38,30 @@ class LoginSerializer(serializers.Serializer):
         pass
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('username', 'email', 'groups')
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('url', 'name')
+        fields = ('id', 'name')
+
+
+class QstringSerializer(serializers.ModelSerializer):
+    count_submissions = serializers.SerializerMethodField()
+
+    def get_count_submissions(self, obj):
+        return obj.submissions.count()
+
+    class Meta:
+        model = Qstring
+        fields = ('id', 'title', 'desc', 'mappingset', 'revision', 'submission_url', 'count_submissions', 'is_active', 'date_created', 'date_updated', 'date_deleted')
+
+
+class SubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Submission
+        fields = ('id', 'sender', 'qstring', 'payload', 'date_created', 'date_updated', 'date_deleted')
