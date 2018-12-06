d6ac3cbf3da0e7ff2af14c64f7e6b24084b74c28
Hope Ogbons <hopeogbons@eHA-Hope-MacBook.local>
2018-12-06 16:27:37 +0100

    updated combineReducers params

--- qstring/client/src/reducers/index.js
+++ qstring/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form';
-import user from './user'
+import user from './user';
+import qstring from './qstring';
 
 export default combineReducers({
-  form: form,
-  user
+  form,
+  user,
+  qstring
 })
