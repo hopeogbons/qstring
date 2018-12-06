902ca70957dd8e7799d3d3151375cb703ae9e080
Hope Ogbons <hopeogbons@eHA-Hope-MacBook.local>
2018-12-06 16:26:11 +0100

    implemented mapDispatchToProps method

--- qstring/client/src/containers/QstringFormContainer.js
+++ qstring/client/src/containers/QstringFormContainer.js
@@ -1,33 +1,58 @@
 import { connect } from 'react-redux';
 import QstringForm from '../components/QstringForm';
-import {createQstringRequest, createQstringSuccess, createQstringFailure} from '../actions/qstrings';
-import { push } from 'connected-react-router';
 import { reduxForm } from 'redux-form';
+import { push } from 'connected-react-router';
+import {
+  createQstringRequest,
+  createQstringSuccess,
+  createQstringFailure,
+  createQstringReset
+} from '../actions/qstrings';
+
+// client side validation
+function validate(values) {
+  const errors = {};
+  if (!(values.title || '').trim()) errors.title = 'Enter a title';
+  if (!(values.desc || '').trim()) errors.desc = 'Enter a description';
+  if (!(values.mappingset || '').trim()) errors.mapping_id = 'Enter a mappingset';
+  if (!(values.revision || '').trim()) errors.revision_id = 'Enter a revision';
+  if (!(values.submission_url || '').trim()) errors.submission_url = 'Enter a submission url';
+  if (!(values.is_active || '').trim()) errors.is_active = 'Select a status';
+  return errors;
+}
+
+function mapStateToProps(state) {
+  return {
+    qstringDetails: state.form.qstring.values
+  };
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    submit: (props) => {
-      dispatch(createQstringRequest(props))
+    submit: (qstringDetails) => {
+      dispatch(createQstringRequest(qstringDetails))
         .then((res) => {
-          const {token, error} = res.payload;
+          const { data, error } = res.payload;
 
-          if (token) {
-            dispatch(createQstringSuccess(token));
+          if (data) {
+            dispatch(createQstringSuccess(data));
             dispatch(push('/'))
           } else {
             dispatch(createQstringFailure(error));
+            dispatch(createQstringReset());
           }
         })
-        .catch(e => {
-          dispatch(createQstringFailure(e));
+        .catch(error => {
+          dispatch(createQstringFailure(error));
+          dispatch(createQstringReset());
         });
     }
   }
 }
 
-const reduxQstringForm = connect(null, mapDispatchToProps)(QstringForm);
+const reduxQstringForm = connect(mapStateToProps, mapDispatchToProps)(QstringForm);
 
 export default reduxForm({
   form: 'qstring',
-  // validate
+  validate
 })(reduxQstringForm);
