479873ffd330c2ae9118a0c56881abe54db52f6d
Hope Ogbons <hopeogbons@eHA-Hope-MacBook.local>
2018-12-06 16:26:35 +0100

    implemented mapDispatchToProps method

--- qstring/client/src/containers/QstringListContainer.js
+++ qstring/client/src/containers/QstringListContainer.js
@@ -1,13 +1,42 @@
 import { connect } from 'react-redux';
 import QstringList from '../components/QstringList';
 import { push } from 'connected-react-router';
+import {
+  fetchQstringRequest,
+  fetchQstringSuccess,
+  fetchQstringFailure,
+  fetchQstringReset
+} from '../actions/qstrings';
+
+function mapStateToProps(state) {
+  return {
+    qstrings: state.qstring.fetchedQstring
+  };
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    gotoQstringForm: () => {
+    loadQstringForm: () => {
       dispatch(push('/create'));
+    },
+    fetchQstrings: () => {
+      dispatch(fetchQstringRequest())
+      .then(res => {
+        const { data, error } = res.payload;
+        if (data) {
+          dispatch(fetchQstringSuccess(data));
+          dispatch(push('/'));
+        } else {
+          dispatch(fetchQstringFailure(error));
+          dispatch(fetchQstringReset());
+        }
+      })
+      .catch(error => {
+        dispatch(fetchQstringFailure(error));
+        dispatch(fetchQstringReset());
+      });
     }
   }
 }
 
-export default connect(null, mapDispatchToProps)(QstringList);
+export default connect(mapStateToProps, mapDispatchToProps)(QstringList);
