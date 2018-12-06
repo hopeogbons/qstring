b74d4b00b1f9341fed2a2a659a378de2166c0543
Hope Ogbons <hopeogbons@eHA-Hope-MacBook.local>
2018-12-06 16:18:56 +0100

    renamed qstring actions

--- qstring/client/src/actions/qstrings.js
+++ qstring/client/src/actions/qstrings.js
@@ -1,44 +1,107 @@
 import axios from 'axios';
 
-// create new qstring
-export const CREATE_QSTRING = 'CREATE_QSTRING';
+// create qstring
+export const CREATE_QSTRING_REQUEST = 'CREATE_QSTRING_REQUEST';
 export const CREATE_QSTRING_SUCCESS = 'CREATE_QSTRING_SUCCESS';
 export const CREATE_QSTRING_FAILURE = 'CREATE_QSTRING_FAILURE';
-export const RESET_NEW_QSTRING = 'RESET_NEW_QSTRING';
+export const CREATE_QSTRING_RESET = 'CREATE_QSTRING_RESET';
+
+// fetch qstring
+export const FETCH_QSTRING_REQUEST = 'FETCH_QSTRING_REQUEST';
+export const FETCH_QSTRING_SUCCESS = 'FETCH_QSTRING_SUCCESS';
+export const FETCH_QSTRING_FAILURE = 'FETCH_QSTRING_FAILURE';
+export const FETCH_QSTRING_RESET = 'FETCH_QSTRING_RESET';
 
 const ROOT_URL = 'http://ui.qstring.local:8500/api/v1';
+const TOKEN = localStorage.getItem('qstring');
 
 export function createQstringRequest(formValues) {
-  const request = axios({
+  const payload = axios({
+    headers: {'Authorization': `Token ${TOKEN}`},
     method: 'post',
     data: formValues,
-    url: `${ROOT_URL}/qstrings`
-  });
+    url: `${ROOT_URL}/qstrings/`
+  })
+    .then(res => {
+      const request = { loading: true };
+      const {data, status} = res;
+      if (data && status === 201) {
+        request.data = data;
+      } else {
+        request.error = 'Unable to create qstring at the moment. Please, try again later.';
+      }
+      return request
+    })
 
   return {
-    type: CREATE_QSTRING,
-    payload: request
+    type: CREATE_QSTRING_REQUEST,
+    payload: payload
   };
 }
+export function createQstringSuccess(data) {
+  const payload = { data: data };
 
-export function createQstringSuccess(newQstring) {
   return {
     type: CREATE_QSTRING_SUCCESS,
-    payload: newQstring
+    payload: payload
   };
 }
-
 export function createQstringFailure(error) {
+  const payload = { error: error };
+
   return {
     type: CREATE_QSTRING_FAILURE,
-    payload: error
+    payload: payload
   };
 }
-
-export function resetNewQstring() {
+export function createQstringReset() {
   return {
-    type: RESET_NEW_QSTRING
+    type: CREATE_QSTRING_RESET
   }
+}
+
+export function fetchQstringRequest() {
+  const payload = axios({
+    headers: {'Authorization': `Token ${TOKEN}`},
+    method: 'get',
+    url: `${ROOT_URL}/qstrings/`
+  })
+    .then(res => {
+      const request = { loading: true };
+      const {data, status} = res;
+      if (data && status === 200) {
+        request.data = data;
+      } else {
+        request.error = 'Unable to fetch qstrings at the moment. Please, try again later.';
+      }
+      return request
+    })
+
+  return {
+    type: FETCH_QSTRING_REQUEST,
+    payload: payload
+  }
+}
+export function fetchQstringSuccess(data) {
+  const payload = { data: data };
+
+  return {
+    type: FETCH_QSTRING_SUCCESS,
+    payload: payload
+  };
+}
+export function fetchQstringFailure(error) {
+  const payload = { error: error };
+
+  return {
+    type: FETCH_QSTRING_FAILURE,
+    payload: payload
+  };
+}
+export function fetchQstringReset() {
+  return {
+    type: FETCH_QSTRING_RESET
+  };
 }
 
 
