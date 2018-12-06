88dd816cda1823aab7cd42dcfb021e0b504113c4
Hope Ogbons <hopeogbons@eHA-Hope-MacBook.local>
2018-12-06 16:30:02 +0100

    added Qstring & Submission classes to the model file

--- qstring/server/models.py
+++ qstring/server/models.py
@@ -1,3 +1,30 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
-# Create your models here.
+class Qstring(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    desc = models.CharField(max_length=100)
+    mappingset = models.CharField(max_length=50)
+    revision = models.CharField(max_length=100)
+    submission_url = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_deleted = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+      return self.title
+
+class Submission(models.Model):
+    sender = models.CharField(max_length=50)
+    qstring = models.ForeignKey(Qstring, related_name='submissions', on_delete=models.CASCADE)
+    payload = JSONField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_deleted = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+      unique_together = ('sender', 'qstring', )
+
+    def __str__(self):
+        return self.sender
